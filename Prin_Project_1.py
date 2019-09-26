7b80cbd1ddb74bc858f724fc94f132a1483ec536
Boa476 <45569169+Boa476@users.noreply.github.com>
2019-09-25 22:54:35 -0500

    Source Code
    executes the translation from .JACK to .XML

---
+++ Prin_Project_1.py
@@ -0,0 +1,21 @@
+"""PRINCIPLES OF PROGRAMMING LANGUAGES PROJECT 1"""
+
+#Open File(s)
+outputFile= "MainT.xml"
+filename= "example.JACK"
+oFile= open(outputFile, 'a')
+file= open(filename, 'r')
+main()
+
+def ParseTree():
+    file.write("<token> \n")
+    for line in file:
+        fjdskal;
+    file.write("</token> \n")
+        
+    
+def main():
+    
+
+
+        
