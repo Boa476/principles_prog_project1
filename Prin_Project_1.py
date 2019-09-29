dfd52464a266a5427bab30e7c48c6c85da8dfb70
Boa476 <45569169+Boa476@users.noreply.github.com>
2019-09-29 14:44:33 -0500

    Prin_Project_1(rev)
    fixed an input/output file declaration and added categories to compare for parsing.

--- Prin_Project_1.py
+++ Prin_Project_1.py
@@ -1,21 +1,28 @@
 """PRINCIPLES OF PROGRAMMING LANGUAGES PROJECT 1"""
-
-#Open File(s)
-outputFile= "MainT.xml"
-filename= "example.JACK"
-oFile= open(outputFile, 'a')
-file= open(filename, 'r')
+#This program is a parse tree for .JACK files
 main()
 
-def ParseTree():
-    file.write("<token> \n")
-    for line in file:
-        fjdskal;
-    file.write("</token> \n")
+def ParseTree(categories):
+    #Start XML 
+    outputFile.write("<token> \n")
+    for char in file:
+        
+    outputFile.write("</token> \n")
         
     
 def main():
+    #define Tokens
+    categories = ['()[]{},;=.+=*/&|~<>', 'class', 'constructor', 'method', 'function', 'int', 'boolean', 'char', 'void', \
+                  'var', 'static', 'field', 'let', 'do', 'if', 'else', 'while', 'return', 'true', 'false', 'null', 'this', \
+                 'abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '0123456789', '_']
+    #Open File(s)
+    outputFile= "MainT.xml"
+    filename= "example.JACK"
+    oFile= open(outputFile, 'a')
+    file= open(filename, 'r')
     
+    #Parse it up!
+    ParseTree(categories)
 
 
         
