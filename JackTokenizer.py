730f1f22b032d02dc88dbbb25db389a43bc4af45
Boa476 <Taylorb043@gmail.com>
2019-10-02 10:54:48 -0500

    Reorganization
    Added test files and began reading of code

--- JackTokenizer.py
+++ JackTokenizer.py
@@ -1,32 +1,43 @@
 """PRINCIPLES OF PROGRAMMING LANGUAGES PROJECT 1"""
 #This is a tokenizer for the Jack Language
-
-   
-def JackTokenizer():
-    #Open Files
-    outputFile="MainT.xml"
-    filename= "Main.JACK"
-    oFile= open(outputFile, 'w')
-    file= open(filename, 'r')
-    file.read()
-    #define Tokens
-    symbols = ['}', '{', ')', '(', ']', '[', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~']
-    keywords = ['class', 'constructor', 'method', 'function', 'int', 'boolean', 'char', 'void', \
-                  'var', 'static', 'field', 'let', 'do', 'if', 'else', 'while', 'return', 'true', 'false', 'null', 'this']
-
-def hasMoreTokens():
+class JackTokenizer:
     
-def advance():
+    def JackTokenizer():
+        #Open Files
+        outputFile="MainT.xml"
+        filename= "Main.jack"
+        oFile= open(outputFile, 'w')
+        file= open(filename, 'r')
+        #define Tokens
+        symbols = ['}', '{', ')', '(', ']', '[', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~']
+        keywords = ['class', 'constructor', 'method', 'function', 'int', 'boolean', 'char', 'void', \
+                    'var', 'static', 'field', 'let', 'do', 'if', 'else', 'while', 'return', 'true', 'false', 'null', 'this']
+         
+        #Temporary testing space
+        with open(filename) as f:
+            for line in f:
+                for word in line.split():
+                    if word == '//':
+                        line++
+ 
+                    print(word)
     
-def tokenType():
 
-def keyWord():
+    """          
+    def hasMoreTokens():
+        
+    def advance():
+        
+    def tokenType():
+        
+    def keyWord():
+        
+    def symbol():
+        
+    def identifier():
+        
+    def intVal():
 
-def symbol():
-    
-def identifier():
-    
-def intVal():
+    def stringVal():
 
-def stringVal():
-    
+"""
