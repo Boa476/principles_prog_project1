40a6cc5f06f4cdfd243495bc8b99c0054203d413
Boa476 <Taylorb043@gmail.com>
2019-10-02 19:34:20 -0500

    Update JackTokenizer.py
    Almost Finished with The Token separation. Attempting to separate the longer strings of many terminal symbols

--- JackTokenizer.py
+++ JackTokenizer.py
@@ -2,28 +2,52 @@
 #This is a tokenizer for the Jack Language
 class JackTokenizer:
     
-    def JackTokenizer():
+    def __init__(self, filename):
         #Open Files
-        outputFile="MainT.xml"
-        filename= "Main.jack"
-        oFile= open(outputFile, 'w')
-        file= open(filename, 'r')
+        self.outputFile = filename + "T.xml"
+        self.filename= filename + ".jack"
+        self.oFile= open(self.outputFile, 'w')
+        self.file= open(self.filename, 'r')
         #define Tokens
-        symbols = ['}', '{', ')', '(', ']', '[', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~']
-        keywords = ['class', 'constructor', 'method', 'function', 'int', 'boolean', 'char', 'void', \
+        self.symbols = [ '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~', '{', '(', '[', ']', ')', '}']
+        self.keywords = ['class', 'constructor', 'method', 'function', 'int', 'boolean', 'char', 'void', \
                     'var', 'static', 'field', 'let', 'do', 'if', 'else', 'while', 'return', 'true', 'false', 'null', 'this']
-         
-        #Temporary testing space
-        with open(filename) as f:
+        self.Tokens =[]
+        
+        #Preparing Tokens
+        comment = False
+        with open(self.filename) as f:
             for line in f:
                 for word in line.split():
-                    if word == '//':
-                        line++
- 
-                    print(word)
+                    if comment == True:
+                        if '*/' not in line:
+                            break
+                        if '*/' in line:
+                            comment = False
+                            break
+                    if '//' in word:
+                        break
+                    if '/*' in word:
+                        comment = True
+                        if '*/' not in line:
+                            break
+                    if comment == False:
+                        done = False
+                        while(done == False):
+                            for i in self.symbols:
+                                if i in word:
+                                    elem = word.split(i, 1)
+                                    self.Tokens.append(elem[0])
+                                    self.Tokens.append(i)
+                                    word = elem[1]
+                                    if elem[1] == '':
+                                        done = True
+                            self.Tokens.append(word)
+        
     
 
-    """          
+    """
+          
     def hasMoreTokens():
         
     def advance():
@@ -41,3 +65,6 @@ class JackTokenizer:
     def stringVal():
 
 """
+a =JackTokenizer('Main')
+for i in a.Tokens:
+    print(a.Tokens[i])
